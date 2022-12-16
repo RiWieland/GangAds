ab7424aa59bad6a767473f7720d37aceceadc6d2
wielandos <rich.wieland73@gmail.com>
2022-12-16 14:12:50 +0100

    added func drawableRGBImage

--- main.go
+++ main.go
@@ -5,29 +5,44 @@ package main
 // https://earthly.dev/blog/golang-errors/
 // - Naming: naming follow frame number
 // - write program for detecting camera-cuts
+// - concurrency in exracting frames?
+// - Sobel-implementation
+// - Modul: Camera switcher
 
 import (
 	"bytes"
 	"fmt"
 	"image"
+	"image/color"
+	"image/draw"
 	"image/jpeg"
 	"io"
+	"log"
 	"os"
 	"strconv"
 
 	"github.com/disintegration/imaging"
 	"github.com/wielandos/sobelfilter"
 
-	//"reflect"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"gocv.io/x/gocv"
 )
 
 var imgSobel image.Image
 
+type MyImg struct {
+	// Embed image.Image so MyImg will implement image.Image
+	// because fields and methods of Image will be promoted:
+	image.Image
+}
+
 func main() {
 
-	imgPath := "./frames_raw/out1961.jpeg"
+	/*
+		ExtractFrames("./video_raw/1.mp4", "./frames_raw/", 324, 327)
+	*/
+
+	imgPath := "./frames_raw/out8133.jpeg"
 	img := gocv.IMRead(imgPath, gocv.IMReadColor)
 	//img := gocv.IMRead(imgPath, gocv.IMReadGrayScale)
 	if img.Empty() {
@@ -50,8 +65,90 @@ func main() {
 
 	imgSobel = InputSobel("./frames_proc/blur_1961.jpg")
 	imgSobel = sobelfilter.ApplySobelFilter(imgSobel) //converts "img" to grayscale and runs edge detect. Returns an image.Image with changes.
-	SaveSobel("./frames_proc/sobel_4.jpg", 90)
+	SaveSobel("./frames_proc/sobel_6.jpg", imgSobel, 180)
 
+	// rotate
+	RotateImg := gocv.NewMat()
+	gocv.Rotate(BlurProcessed, &RotateImg, gocv.Rotate90CounterClockwise)
+	SaveFile("./frames_proc/rotate_1961.jpg", RotateImg)
+
+	f, err := os.Open("./frames_proc/rotate_1961.jpg")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img_, err := drawableRGBImage(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Model:", img_.ColorModel())
+	fmt.Println("Bounds:", img_.Bounds())
+	fmt.Println("At(1,2):", img_.At(1, 2))
+	img_.Set(1, 2, color.White)
+	fmt.Println("At(1,2):", img_.At(1, 2), "(after Set)")
+
+}
+
+func (m *MyImg) At(x, y int) color.Color {
+	// "Changed" part: custom colors for specific coordinates:
+	switch {
+	case x == 0 && y == 0:
+		return color.RGBA{85, 165, 34, 255}
+	case x == 0 && y == 1:
+		return color.RGBA{255, 0, 0, 255}
+	}
+	// "Unchanged" part: the colors of the original image:
+	return m.Image.At(x, y)
+}
+
+func drawableRGBImage(f io.Reader) (draw.Image, error) {
+	img, err := jpeg.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	b := img.Bounds()
+	output_rgb := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+	draw.Draw(output_rgb, output_rgb.Bounds(), img, b.Min, draw.Src)
+
+	return output_rgb, nil
+}
+
+// Get the bi-dimensional pixel array
+func getPixels(file io.Reader) ([][]Pixel, error) {
+	img, _, err := image.Decode(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	var pixels [][]Pixel
+	for y := 0; y < height; y++ {
+		var row []Pixel
+		for x := 0; x < width; x++ {
+			row = append(row, rgbaToPixel(img.At(x, y).RGBA()))
+		}
+		pixels = append(pixels, row)
+	}
+
+	return pixels, nil
+}
+
+// img.At(x, y).RGBA() returns four uint32 values; we want a Pixel
+func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
+	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
+}
+
+// Pixel struct example
+type Pixel struct {
+	R int
+	G int
+	B int
+	A int
 }
 
 func InputSobel(inputPath string) image.Image {
@@ -68,7 +165,7 @@ func InputSobel(inputPath string) image.Image {
 	return imgInputSobel
 }
 
-func SaveSobel(targetPath string, quality int) {
+func SaveSobel(targetPath string, img image.Image, quality int) {
 	e, err := os.Create(targetPath)
 	if err != nil {
 		fmt.Println("exist already")
@@ -77,7 +174,7 @@ func SaveSobel(targetPath string, quality int) {
 	opt := jpeg.Options{
 		Quality: quality,
 	}
-	err = jpeg.Encode(e, imgSobel, &opt)
+	err = jpeg.Encode(e, img, &opt)
 	if err != nil {
 		// Handle error
 	}
